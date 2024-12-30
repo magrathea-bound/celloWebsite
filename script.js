837d6444804e77ae44a80446960fa6eaec33e214
magrathea-bound <dg.burns@outlook.com>
2024-12-30 12:01:05 -0600

    Main image and bio functional, trying to add second parallax

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
 
+// Navbar fill in when scrolling down
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 500) {  
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+// parallax scrolling function
+
+let parallaxImages = []
+parallaxImages.push(document.getElementById('main-image'));
+parallaxImages.push(document.getElementById('recording-image'));
+
+window.addEventListener('scroll', function() {
+    const scrollPosition = window.scrollY;
+
+    for (let parallaxImage of parallaxImages) {
+    parallaxImage.style.transform = `translateY(${scrollPosition * 0.3}px)`;
+}
+});
 
 
+// Bio Expansion
+const showMoreButton = document.getElementById('bioShowMoreBtn');
+const hiddenContent = document.querySelector('.hidden');
 
+// Add a click event listener to the button
+showMoreButton.addEventListener('click', () => {
+    if (hiddenContent.style.maxHeight){
+        hiddenContent.style.maxHeight = null;
+        showMoreButton.textContent = 'Show Less';
+    } else{
+        hiddenContent.style.maxHeight = hiddenContent.scrollHeight + "px";
+        showMoreButton.textContent = 'Show More';
+
+    }
+
+});
 
 //Old Scripts for reference
 //Link Switches
